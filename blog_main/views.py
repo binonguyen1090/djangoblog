780eb5864cfa5bbb3b93a45c920e5085736d39ca
bino.nguyen <bino.nguyen@teamww.com>
2026-05-06 22:49:13 -0500

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,15 +1,9 @@
-
-
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 
 def home(request):
-    return render(request,'home.html')
-
-
-
-
-
-
-
-
+    featured_posts = Blog.objects.filter(is_featured=True)
+    posts = Blog.objects.filter(is_featured=False)
+    context = {'featured_posts': featured_posts, 'posts': posts}
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Blog, Category
 
-# Create your views here.
+
+
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(category=category_id)
+    # category = get_object_or_404(Category, pk=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return render(request, '404.html')
+    context = {'posts': posts, 'category': category}
+    return render(request, 'posts_by_category.html', context)
